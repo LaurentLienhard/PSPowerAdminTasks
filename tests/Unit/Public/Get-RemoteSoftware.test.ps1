f927434e185d74a41b679feb2eb709302bd09803
LaurentLienhard <laurent.lienhard@outook.com>
2025-11-19 12:08:01 +0100

    update

---
+++ tests/Unit/Public/Get-RemoteSoftware.test.ps1
@@ -0,0 +1,13 @@
+﻿BeforeAll {
+    $script:dscModuleName = 'PSPowerAdminTasks'
+
+    Import-Module -Name $script:dscModuleName
+}
+
+AfterAll {
+    # Unload the module being tested so that it doesn't impact any other tests.
+    Get-Module -Name $script:dscModuleName -All | Remove-Module -Force
+}
+
+Describe Get-RemoteSoftware {
+}
