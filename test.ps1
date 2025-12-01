4e892e8ff4b785cb6990e2f80dec9c7bcdb75ce5
LaurentLienhard <laurent.lienhard@outook.com>
2025-12-01 11:26:55 +0100

    erroro

--- test.ps1
+++ test.ps1
@@ -1,7 +1,1 @@
-﻿Set-RemoteDnsDebugLog: C:\Users\llienhard\Documents\01-DEV\Github\PSPowerAdminTasks\source\Public\Set-RemoteDnsDebugLog.ps1:152:2
-Line |
- 152 |   Set-RemoteDnsDebugLog -ComputerName ecrdc01 -Credential (Get-Secret  …
-     |   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     | Error configuring DNS on ecrdc01 : Parameter cannot be processed because the parameter name 'EnableLogging' is ambiguous. Possible matches include:
-     | -EnableLoggingForRemoteServerEvent -EnableLoggingForPluginDllEvent -EnableLoggingForZoneLoadingEvent -EnableLoggingForLocalLookupEvent -EnableLoggingToFile
-     | -EnableLoggingForZoneDataWriteEvent -EnableLoggingForTombstoneEvent -EnableLoggingForRecursiveLookupEvent -EnableLoggingForServerStartStopEvent.
+﻿Set-RemoteDnsDebugLog: Error configuring DNS on ecrdc01 : A parameter cannot be found that matches parameter name 'PacketDirection'.
