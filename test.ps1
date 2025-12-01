868aabbf2495bcd13e1b66dc013cf773f5fed2d5
LaurentLienhard <laurent.lienhard@outook.com>
2025-12-01 11:43:52 +0100

    error

--- test.ps1
+++ test.ps1
@@ -1,1 +1,7 @@
-﻿Set-RemoteDnsDebugLog: Error configuring DNS on ecrdc01 : A parameter cannot be found that matches parameter name 'PacketDirection'.
+﻿VERBOSE: Starting AD Sites query...
+VERBOSE: Querying sites with filter: *
+VERBOSE: Processing site: DSD
+VERBOSE: Added 18 inter-site link(s) with cost information to site: DSD
+VERBOSE: Total inter-site cost: 2100
+Get-SiteInformation: Failed to retrieve site information for '*': Cannot find an overload for "Add" and the argument count: "1".
+VERBOSE: Query complete. Returning 0 site(s).
