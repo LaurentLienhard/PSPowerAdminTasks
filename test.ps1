44198509fe23e49b59ce811165ca3ed6d23aa42f
LaurentLienhard <laurent.lienhard@outlook.com>
2026-02-06 10:09:27 +0100

    push

--- test.ps1
+++ test.ps1
@@ -2,7 +2,3 @@
 Import-Module -FullyQualifiedName .\output\module\PSPowerAdminTasks\0.0.1\PSPowerAdminTasks.psm1 -Force
 
  Get-adComputer -Filter 'OperatingSystem -like "*Server*" -and Enabled -eq $true' -Properties Name | Select-Object -ExpandProperty Name | Send-MailForAreboot -Recipient llienhard@cw.fmlogistic.com -Credential (Get-Secret AdmAccount) -Verbose
-
-
- PS C:\Users\llienhard\Documents\01-DEV\Github\PSPowerAdminTasks> .\build.ps1 -Tasks deploy_remote -credential (Get-Secret AdmAccount)
-build.ps1: A parameter cannot be found that matches parameter name 'credential'.
