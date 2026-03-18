275eb9f9ace742839c384228fb20f12af7e2d875
LaurentLienhard <laurent.lienhard@outlook.com>
2026-03-18 15:23:51 +0100

    update fucntion

--- source/Public/Get-LocalAdministratorRemotly.ps1
+++ source/Public/Get-LocalAdministratorRemotly.ps1
@@ -1,5 +1,54 @@
 ﻿function Get-LocalAdministratorsRemotly
 {
+    <#
+        .SYNOPSIS
+            Retrieves the members of the local Administrators group from one or more remote computers.
+
+        .DESCRIPTION
+            This function queries remote computers to retrieve all members of the local Administrators group.
+            It supports credential-based authentication and optional logging of results. The function handles
+            special cases where SIDs are present in the group membership and provides detailed object information
+            including computer name, OS version, member name, object class, and principal source.
+
+        .PARAMETER ComputerName
+            The name(s) of the remote computer(s) to query. This parameter accepts an array of computer names
+            and is mandatory. The function will connect to each computer to retrieve administrator group members.
+
+        .PARAMETER Credential
+            Optional credentials to use when connecting to the remote computer(s). If not specified, the
+            current user's credentials will be used for the remote connection.
+
+        .PARAMETER LogPath
+            The path where the log file will be saved. Default is $env:Temp/Get-LocalAdministratorsRemotly.log.
+            Only used if the log parameter is specified.
+
+        .PARAMETER log
+            If specified, logs the results to a CSV file at the path specified by LogPath parameter.
+            Default is $false.
+
+        .OUTPUTS
+            PSCustomObject[] with the following properties:
+            - ComputerName: The name of the remote computer
+            - OSVersion: The operating system version of the remote computer
+            - Member: The name of the local administrator
+            - ObjectClass: The object class of the member (e.g., User, Group)
+            - PrincipalSource: The source of the principal (e.g., Local, ActiveDirectory)
+
+        .EXAMPLE
+            Get-LocalAdministratorsRemotly -ComputerName 'SERVER01'
+
+            Retrieves all local administrators from SERVER01 using the current user's credentials.
+
+        .EXAMPLE
+            Get-LocalAdministratorsRemotly -ComputerName 'SERVER01', 'SERVER02', 'SERVER03' -Credential $cred
+
+            Retrieves local administrators from multiple servers using the specified credentials.
+
+        .EXAMPLE
+            Get-LocalAdministratorsRemotly -ComputerName 'SERVER01' -log
+
+            Retrieves local administrators and logs the results to C:\rundeck\Get-LocalAdministratorsRemotly.csv.
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
@@ -10,16 +59,14 @@
         [System.Management.Automation.Credential()]
         $Credential = [System.Management.Automation.PSCredential]::Empty,
         [Parameter()]
+        [System.String]$LogPath = "$env:Temp/Get-LocalAdministratorsRemotly.log",
+        [Parameter()]
         [switch]$log
     )
 
     begin
     {
-        if ($log)
-        {
-            $LogPath = 'C:\rundeck\Get-LocalAdministratorsRemotly.csv'
-        }
-
+        Write-Verbose "Starting Get-LocalAdministratorsRemotly"
     }
 
     process
